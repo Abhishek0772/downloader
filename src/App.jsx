f87b40310fb6656efcae1ec9bcc707b989aaf6c4
abhi <satputeabhishek2888@gmail.com>
2022-11-24 02:31:34 -0800

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React ,{useState} from 'react'
+import React, { useState } from 'react'
 
 function App() {
-const [download , setdownload] = useState('');
-const values = download?  true : false
+  const [download, setdownload] = useState('');
+  const values = download ? true : false
 
-console.log(values)
+  console.log(values)
   return (
     <div className='h-screen w-full bg-slate-200 flex justify-center items-center'>
 
       <div className='flex justify-center items-center flex-col gap-4 bg-gradient-to-br from-purple-600 to-pink-400 h-80 p-4 rounded-lg shadow-xl shadow-gray-400'>
-        <input type="text" onChange={(e)=>{
+        <input type="text" onChange={(e) => {
           setdownload(e.target.value)
-        }} required className='border-[1px] border-black py-2 w-72 rounded-md bg-transparent' placeholder='enter you want to download text-white'/>
-        <button className='border-[1px] border-black p-2 my-4 text-white'><a href={download} download={values}>
+        }} required className='border-[1px] border-black py-2 w-72 rounded-md bg-transparent' placeholder='enter you want to download text-white' />
+        <button download={values} className='border-[1px] border-black p-2 my-4 text-white'><a href={download} >
           DOWNLOAD</a></button>
       </div>
-        
+
 
     </div>
   )
