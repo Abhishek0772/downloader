e42b96d620772d6b4b1bec4e639efc137fe20f5f
abhi <satputeabhishek2888@gmail.com>
2022-11-24 01:58:37 -0800

    first

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React ,{useState} from 'react'
+
+function App() {
+const [download , setdownload] = useState('');
+const values = download?  true : false
+
+console.log(values)
+  return (
+    <div className='h-screen w-full bg-slate-200 flex justify-center items-center'>
+
+      <div className='flex justify-center items-center flex-col gap-4 bg-gradient-to-br from-purple-600 to-pink-400 h-80 p-4 rounded-lg '>
+        <input type="text" onChange={(e)=>{
+          setdownload(e.target.value)
+        }} required className='border-[1px] border-black py-2 w-72 rounded-md bg-transparent' placeholder='enter you want to download text-white'/>
+        <button className='border-[1px] border-black p-2 my-4'><a href='' download={values}>
+          DOWNLOAD</a></button>
+      </div>
+        
+
+    </div>
+  )
+}
+
+export default App
